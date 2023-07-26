7c6845136664664c60a8d6a2a6dafeab95a5a8b5
Arginanta <arginanta30@gmail.com>
2023-07-26 23:49:02 +0700

    React pertama

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer(params) {
+  return (
+    <footer className="flex text-center px-4 py-8 justify-center">
+      <p className="text-sm">
+        Copyright 2021 • All Rights Reserved LuxSpace by Muhammad Arginanta Kafi Sambada
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
